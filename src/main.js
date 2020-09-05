3317f5fef02a80d6b412cc681464b161a8fd8936
liujianxiong <liujianxiong@issmart.com.cn>
2020-09-05 22:26:28 +0800

    add progress

--- src/main.js
+++ src/main.js
@@ -6,10 +6,17 @@ import store from './store'
 import '@/assets/scss/style.scss'
 import './plugins/element.js'
 import axios from 'axios'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
 
 axios.interceptors.request.use(config => {
+  NProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+axios.interceptors.response.use(config => {
+  NProgress.done()
   return config
 })
 Vue.prototype.$http = axios
@@ -20,7 +27,6 @@ Vue.filter('dateFormat', function(originVal) {
   const dt = new Date(originVal)
   const y = dt.getFullYear()
   // 不足两位用零补齐
-
   const m = (dt.getMonth() + 1 + '').padStart(2, '0')
   const d = (dt.getDate() + '').padStart(2, '0')
   const hh = (dt.getHours() + '').padStart(2, '0')
